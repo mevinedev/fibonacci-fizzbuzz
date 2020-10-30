940c5530f34f4461042029ea65e44d1753368fd9
mevinedev <72660182+mevinedev@users.noreply.github.com>
2020-10-30 10:45:16 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,9 @@
 n = int(input("integer: "))
 if n>=1:
     def fibonacci(n):
-        if n==1:
+        if n == 1:
             return 0
-        elif n==2: 
+        elif n == 2: 
             return 1
         elif n > 2:
             return fibonacci(n-1) + fibonacci(n-2)
